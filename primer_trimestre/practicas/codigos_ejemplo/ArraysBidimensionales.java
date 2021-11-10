95882891c77a2b5b2c38400776606a598ecdcdd5
dammonmarrod <68920289+dammonmarrod@users.noreply.github.com>
2021-11-10 04:53:26 +0100

    Create ArraysBidimensionales.java

---
+++ primer_trimestre/practicas/codigos_ejemplo/ArraysBidimensionales.java
@@ -0,0 +1,28 @@
+
+package paquete;
+
+import java.util.Scanner;
+
+public class Clase3 {
+
+  
+    public static void main(String[] args) {
+ 
+            int array2d[][] = new int[10][2];
+            int otro2d[][] = {{1,2,3,4},{5,7,8,7,6,4,3,2,2},{9,0,1,2}};
+            
+            for (int i = 0; i < otro2d.length; i++) {
+                for (int j = 0; j < otro2d[i].length; j++) {
+                    System.out.print(otro2d[i][j]+" ");
+                }
+                System.out.println();
+            }
+            
+            int array3d[][][] = new int [10][4][5];
+            int array4d[][][][] = new int [4][3][7][3];
+            
+            
+
+    }
+ 
+}
