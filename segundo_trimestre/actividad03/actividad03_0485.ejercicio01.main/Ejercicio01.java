3c6b6e70637442dfc9262cb99182fcfcf75a4118
dammonmarrod <68920289+dammonmarrod@users.noreply.github.com>
2021-12-19 11:47:43 +0100

    Update Ejercicio01.java

--- segundo_trimestre/actividad03/actividad03_0485.ejercicio01.main/Ejercicio01.java
+++ segundo_trimestre/actividad03/actividad03_0485.ejercicio01.main/Ejercicio01.java
@@ -1,6 +1,10 @@
 package actividad03_0485.ejercicio01.main;
 
 import actividad03_0485.ejercicio01.introduceDatos.Pregunta;
+import actividad03_0485.ejercicio01.operaciones.Valores;
+import actividad03_0485.ejercicio01.operaciones.algebraicas.Operaciones;
+import actividad03_0485.ejercicio01.operaciones.geometricas.Operacion;
+
 import java.util.Scanner;
         
 public class Ejercicio01 {
@@ -21,26 +25,35 @@ public class Ejercicio01 {
            opcion=Pregunta.pideEntero("Elige una de estas opciones: ");
            switch(opcion){
                case 1:
-                   System.out.println("Has seleccionado la opcion 1");
+                   System.out.println("Has seleccionado la opcion 1.Muestra Pi.");
+                   Valores.muestraPi();
                    break;
                case 2:
-                   System.out.println("Has seleccionado la opcion 2");
+                   System.out.println("Has seleccionado la opcion 2. Muestra valor aleatorio.");
+                   Valores.muestraValorAleatorio();
                    break;
                 case 3:
-                   System.out.println("Has seleccionado la opcion 3");
+                   System.out.println("Has seleccionado la opcion 3. Muestra raiz cuadrada.");
+                   Operaciones.muestraRaizCuadrada();
                    break;
                 case 4:
-                    System.out.println("Has seleccionado la opcion 4");
+                    System.out.println("Has seleccionado la opcion 4. Calcula potencia.");
+                    double valor1=Pregunta.pideDouble("Introduce el primer valor double:");
+                    double valor2=Pregunta.pideDouble("Introduce el segundo valor double:");
+                    Operaciones.calculaPotencia(valor1, valor2);
                    break;
                 case 5:
-                    System.out.println("Has seleccionado la opcion 5");
+                    System.out.println("Has seleccionado la opcion 5. Muestra Seno");
+                    Operacion.muestraSeno();
                    break;
                 case 6:
                    System.out.println("Has seleccionado la opcion 6");
+                   Operacion.muestraCoseno();
                    break;
                 default:
                    System.out.println("Solo números entre 1 y 6");
            }
+          System.out.println(""); 
        }
       while(opcion !=0);
     }
