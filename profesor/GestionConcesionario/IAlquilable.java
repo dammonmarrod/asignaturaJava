e50d15ccfbc4f6a6bdb5a9b7a3207ac15a2fcd5e
dammonmarrod <68920289+dammonmarrod@users.noreply.github.com>
2022-03-12 07:57:07 +0100

    Create IAlquilable.java

---
+++ profesor/GestionConcesionario/IAlquilable.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad04_0485.ejercicio03.vehiculos;
+
+/**
+ *
+ * @author Cristian
+ */
+public interface IAlquilable {
+      
+      
+      public double getPrecioTotalAlquilerPorDias ();
+      public void setNumeroDias(int n);
+  
+      
+      
+      
+}
